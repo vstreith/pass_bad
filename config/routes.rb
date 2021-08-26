9b9de70b89b096232db1e26d7e87f70be2dc12eb
vstreith <victor.streith@gmail.com>
2021-08-26 11:18:07 +0200

    Modification Devise pour confirmation mail

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :path=>'',
                     :path_names=>{:sign_in=>'login', :sign_out=>'logout', :edit=>'profile'},
-                    :controllers=>{:registrations=>'registrations'}
+                    :controllers=>{:registrations=>'registrations', :confirmations=>'confirmations'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_070729) do
+ActiveRecord::Schema.define(version: 2021_08_26_090711) do
 
   create_table "clubs", force: :cascade do |t|
     t.string "club_name"
@@ -94,6 +94,10 @@ ActiveRecord::Schema.define(version: 2021_08_26_070729) do
     t.datetime "avatar_updated_at"
     t.string "licence_number"
     t.text "club"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
