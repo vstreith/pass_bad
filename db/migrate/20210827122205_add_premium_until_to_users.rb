beb7b639693c0c929a4125702a6dbff7c974c154
vstreith <victor.streith@gmail.com>
2021-09-06 12:04:47 +0200

    Ajout automatique d'un mois gratuit à l'inscrition

--- db/migrate/20210827122205_add_premium_until_to_users.rb
+++ db/migrate/20210827122205_add_premium_until_to_users.rb
@@ -2,4 +2,8 @@ class AddPremiumUntilToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :premium_until, :date
   end
+  def up
+  premium_until_offer = DateTime.now + 1.month
+  User.all.each { |user| user.update! premium_until: premium_until_offer }
+  end
 end
