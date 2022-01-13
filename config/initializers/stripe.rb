ea7d9553f50d7b33c6651d92f622f2f284a31f57
vstreith <victor.streith@gmail.com>
2022-01-13 12:52:46 +0100

    maj stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,2 @@
 # config/initializers/stripe.rb
-Rails.configuration.stripe = {
-:publishable_key => ENV['PUBLISHABLE_KEY'],
-:secret_key => ENV['SECRET_KEY']
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = Rails.application.credentials[:stripe][:secret_key]
