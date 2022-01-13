9e8066dd5e755a1d8dd298b7b78a47959893b202
vstreith <victor.streith@gmail.com>
2022-01-13 15:04:50 +0100

    modif config production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,8 @@ Rails.application.configure do
     })
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'lit-hamlet-43553.herokuapp.com', :protocol =>'https'}
 
