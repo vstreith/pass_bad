a09e06fa0c35dba2c2902ada2271a2af452499f8
vstreith <victor.streith@gmail.com>
2022-01-26 19:09:59 +0100

    Mise à jour Stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,7 @@
 # config/initializers/stripe.rb
 Rails.configuration.stripe = {
-    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-    :secret_key => ENV['STRIPE_SECRET_KEY']
+  publishable_key: Rails.application.secrets.stripe[:publishable_key],
+  secret_key: Rails.application.secrets.stripe[:secret_key]
 }
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+Stripe.api_key = Rails.application.secrets.stripe[:secret_key]
